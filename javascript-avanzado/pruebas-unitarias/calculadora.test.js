ffd5eb4cbac4361303bdf5fd754922efaf1239b9
LuisAguilarG <95900211+LuisAguilarG@users.noreply.github.com>
2022-01-25 19:16:27 -0600

    Añade ejercicio de pruebas unitarias

--- javascript-avanzado/pruebas-unitarias/calculadora.test.js
+++ javascript-avanzado/pruebas-unitarias/calculadora.test.js
@@ -3,8 +3,7 @@
 
 // Como es javascript de lado del servidor (node js), no se utiliza el import, sino:
 // En el otro documento se exportó como un objeto, por lo que también se debe importar como un objeto utilizando llaves. 
-const {sumar} = require('./calculadora');
-const {restar} = require('./calculadora');
+const {sumar, restar, multiplicar, dividir} = require('./calculadora');
 
 
 // A qué archivo estamos haciéndole una prueba.
@@ -18,6 +17,16 @@ describe('Pruebas en el componente calculadora', () => {
     test('Pruebas en la función restar', () => {
         // Solo importa la entrada y salida esperada
         expect(restar(3, 5)).toBe(-2);
+    });
+
+    test('Pruebas en la función multiplicar', () => {
+        // Solo importa la entrada y salida esperada
+        expect(multiplicar(3, 5)).toBe(15);
+    });
+
+    test('Pruebas en la función dividir', () => {
+        // Solo importa la entrada y salida esperada
+        expect(dividir(100, 2)).toBe(50);
     });
 
 
