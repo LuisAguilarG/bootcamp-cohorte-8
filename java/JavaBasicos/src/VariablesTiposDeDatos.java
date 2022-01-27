2e8bd49c5c5cc56addc0eab59c37ccf199005b9f
LuisAguilarG <95900211+LuisAguilarG@users.noreply.github.com>
2022-01-26 18:48:51 -0600

    Agrega introduccion de Java

---
+++ java/JavaBasicos/src/VariablesTiposDeDatos.java
@@ -0,0 +1,40 @@
+
+public class VariablesTiposDeDatos {
+
+	public static void main(String[] args) {
+		
+		// Cadena de texto
+		String nombre = "Luis Aguilar";
+		
+		// Char, un caracter
+		int edad = 22;
+		
+		// Boolean
+		boolean tienesCrush = false; 
+		
+		// Tipos de datos enteros
+		
+		// byte
+		byte edadDos = 31;
+		// Da error ya que es mayor a 127
+		// byte suma = 24 + 104; 
+		
+		// short
+		short suma = 24 + 104;
+		
+		// int 
+		int numero = 105545455;
+		
+		// long, podemos separarlos con guions bajos para mejorar legibilidad
+		long numeroLargo = 100_000_000;
+		
+		
+		System.out.println(numeroLargo);
+		
+		// Float, lleva una f al final
+		float promedio = 1232.23232f;
+		
+		// double
+		double numeroPi = 3.14159545655; 
+	}
+}
